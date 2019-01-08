869655c1c107b71536f6900748b5b89a2c75b30d
Jimil <shahjimil35@gmail.com>
2019-01-08 10:49:09 +0530

    Updated properties

--- src/main/java/com/springboot/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/springboot/eurekaclient/EurekaClientApplication.java
@@ -2,15 +2,17 @@ package com.springboot.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
-
 }
-
